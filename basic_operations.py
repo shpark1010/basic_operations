af23e012ae2a6e02afc03ded50d6076a4c704c51
Sang Hyun Park <88411966+shpark1010@users.noreply.github.com>
2022-03-12 23:52:57 +0900

    update hello world

--- basic_operations.py
+++ basic_operations.py
@@ -6,15 +6,3 @@ with tf.compat.v1.Session() as sess:
   hw = h + w
   ans = sess.run(hw)
   print(ans)
-
-g = tf.Graph() 
-with g.as_default():   
-	a = tf.constant(3, name="a")
-	b = tf.constant(2, name="b")
-	c = tf.constant(1, name="c")
-
-	d = a * b
-	e = d + c
-
-sess = tf.compat.v1.Session(graph=g)
-print(sess.run(e))
