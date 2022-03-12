11c71e3340df132265e0e81565437d5500782b21
Sang Hyun Park <88411966+shpark1010@users.noreply.github.com>
2022-03-12 23:49:13 +0900

    graph tensorflow
    graph tensorflow

--- basic_operations.py
+++ basic_operations.py
@@ -1,8 +1,13 @@
 import tensorflow as tf
 
-with tf.compat.v1.Session() as sess:
-  h = tf.constant("Hello")
-  w = tf.constant("World!")
-  hw = h + w
-  ans = sess.run(hw)
-  print(ans)
+g = tf.Graph() 
+with g.as_default():   
+	a = tf.constant(3, name="a")
+	b = tf.constant(2, name="b")
+	c = tf.constant(1, name="c")
+
+	d = a * b
+	e = d + c
+
+sess = tf.compat.v1.Session(graph=g)
+print(sess.run(e))
